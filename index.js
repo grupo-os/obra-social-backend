91a2ddaa59c7dcbb04400e86d57fd4b840edc4bf
gonnzalezemma <jorge_99.l@hotmail.com>
2021-10-21 17:58:16 -0300

    conexion y db conectados y funcionando

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hola");
+const express = require('express');
+
+
+//inicializaciones
+const app = express();
+require('dotenv').config();
+require('./connection');
+
+//middlewares- -
+
+//conexion al server
+app.set("port", process.env.PORT || 5000);
+
+
+
+//devuelve la conexion
+app.listen(app.get("port"),()=>
+    console.log(`server on port ${app.get("port")}`)
+);
